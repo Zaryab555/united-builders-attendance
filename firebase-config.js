c9b50d65a650c54466e007a1878f3a5f7b72d888
Zaryab555 <zaryabrashid04@gmail.com>
2026-07-23 22:39:43 +0100

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAhI0NSEMKOQsPTTULNBA4__1TbCh6gwsU",
+  authDomain: "united-builders-attendance.firebaseapp.com",
+  projectId: "united-builders-attendance",
+  storageBucket: "united-builders-attendance.firebasestorage.app",
+  messagingSenderId: "1055668010937",
+  appId: "1:1055668010937:web:599f7bd91eae66714b8068",
+  measurementId: "G-8WFL1T8K4B"
+};
